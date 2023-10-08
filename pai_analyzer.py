1398c538d9ad66481a96aa90abe3dc67af54669d
Alex Lunacharskii <alun@katlex.com>
2023-10-08 15:40:40 +0100

    Updated logic

--- pai_analyzer.py
+++ pai_analyzer.py
@@ -3,6 +3,7 @@ Streamlit app for Perceptrader monitoring and analysis
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -14,9 +15,13 @@ currency_sym = st.text_input("Deposit currency symbol", value="€")
 assumed_capital = float(st.text_input("Assumed starting capital", value="1000"))
 
 data = pd.read_csv(data_url, skiprows=1)
-pai_trades = data[
-    data["Order comment"].str.contains(comment_filter).fillna(False)
-].astype({"Close time": "datetime64[ns]", "Open time": "datetime64[ns]"})
+
+filter_mask = (
+    data["Order comment"].str.contains(comment_filter).fillna(not comment_filter)
+)
+trades = data[filter_mask].astype(
+    {"Close time": "datetime64[ns]", "Open time": "datetime64[ns]"}
+)
 
 columns = [
     # "Type",
@@ -52,13 +57,13 @@ skip_columns_open = [
 ]
 
 
-open_trades_mask = pai_trades["Type"] == "Open position"
-closed_trades_mask = pai_trades["Type"] == "Closed position"
+open_trades_mask = trades["Type"] == "Open position"
+closed_trades_mask = trades["Type"] == "Closed position"
 
-open_trades = pai_trades[open_trades_mask][
+open_trades = trades[open_trades_mask][
     [col for col in columns if col not in skip_columns_open]
 ].reset_index(drop=True)
-closed_trades = pai_trades[closed_trades_mask][columns].reset_index(drop=True)
+closed_trades = trades[closed_trades_mask][columns].reset_index(drop=True)
 
 st.header("Open trades")
 st.write(open_trades[::-1])
@@ -66,7 +71,7 @@ st.write(open_trades[::-1])
 st.header("Closed trades")
 st.write(closed_trades[::-1])
 
-st.header("Profitability")
+st.header("Statistics")
 
 closed_profit = closed_trades["Net profit"].sum()
 closed_profit_pct = 100 * closed_profit / assumed_capital
@@ -76,19 +81,38 @@ first_ts = closed_trades["Open time"].min()
 last_ts = closed_trades["Close time"].max()
 days_total = (last_ts - first_ts).days
 
-col1, col2 = st.columns([1, 3], gap="small")
+negative_mask = closed_trades["Net profit"] < 0
+lost_loss = -closed_trades["Net profit"][negative_mask].sum()
+won_profit = closed_trades["Net profit"][~negative_mask].sum()
 
-col1.write("Began trading")
-col2.write(str(first_ts))
-col1.write("Last closed trade")
-col2.write(str(last_ts))
-col1.write("Total days running")
-col2.write(str(days_total))
-col1.write("Closed profit/loss")
-col2.write(f"{closed_profit:.2f}{currency_sym} ({closed_profit_pct:.2f}%)")
-col1.write("Open profit/loss")
-col2.write(f"{open_profit:.2f}{currency_sym} ({open_profit_pct:.2f}%)")
+lots = trades["Lots"].sum()
+annual = (1 + closed_profit / assumed_capital) ** (365 / days_total) - 1
 
+st.write("**Net profit/loss is mentioned below, unless otherwise specified**")
+
+
+def t_row(header, value, comment=None):
+    col1, col2, col3 = st.columns([1, 1, 2], gap="small")
+    col1.write("**" + header + "**")
+    col2.write(str(value))
+    if comment:
+        col3.write(str(comment))
+
+
+t_row("Began trading", first_ts)
+t_row("Last closed trade", last_ts)
+t_row("Total days running", days_total)
+t_row("Total trades", len(closed_trades))
+t_row(
+    "Closed profit/loss",
+    f"{closed_profit:.2f}{currency_sym} ({closed_profit_pct:.2f}%)",
+)
+t_row("Traded lots", f"{lots:.2f}")
+t_row("Profit/loss per 0.01 lot", f"{closed_profit / (lots / 0.01):.2f}{currency_sym}")
+t_row("Profit factor", f"{won_profit / lost_loss:.2f}")
+t_row("Open profit/loss", f"{open_profit:.2f}{currency_sym} ({open_profit_pct:.2f}%)")
+t_row("Approximate gain", f"{closed_profit / assumed_capital * 100:.2f}%")
+t_row("Annualized gain", f"{annual * 100:.2f}%")
 
 closed_trades["Net profit"].cumsum().plot()
 plt.title("Closed profit/loss")
@@ -102,11 +126,26 @@ st.header("Cost of trading")
 col1, col2, _ = st.columns([1, 1, 2], gap="small")
 
 fees = closed_trades["Swap"].sum() + closed_trades["Commission"].sum()
-col1.write("Fees")
-col2.write(str(fees) + currency_sym)
-
 closed_profit_gross = closed_trades["Profit"].sum()
-col1, col2, col3 = st.columns([1, 1, 2], gap="small")
-col1.write("% of profit")
-col2.write(f"{-100 * fees/ closed_profit_gross:.2f}%")
-col3.write("<- % of gross profit shared with the broker")
+fees_pct = -100 * fees / closed_profit_gross
+
+col1.write("Fees")
+col2.write(f"{-fees:.2f}{currency_sym}")
+
+if fees_pct >= 0:
+    col1, col2, col3 = st.columns([1, 1, 2], gap="small")
+
+    col1.write("% of profit")
+    col2.write(f"{fees_pct:.2f}%")
+    col3.write("<- % of gross profit shared with the broker")
+
+
+st.header("By symbol")
+
+st.dataframe(
+    closed_trades.groupby("Symbol")
+    .apply(
+        lambda df: pd.Series({"Lots": df["Lots"].sum(), "Count": df["Lots"].count()})
+    )
+    .astype({"Count": "int32"})
+)
